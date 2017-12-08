64ced82661c4e1efef9ff2986609d5a7686c7f34
Quan Do <quan.dm@teko.vn>
2017-12-08 05:49:39 +0000

    update

--- app/Engine/TestThread.php
+++ app/Engine/TestThread.php
@@ -41,7 +41,7 @@ class TestThread extends \Thread
                 preg_match("/location.href\s*=\s*[\"'](.*)[\"']/i", $result, $value))) {
             return $this->virtualCurl($isoCode, $value[1], $userAgent, ++$currentRedirection);
         } else {
-            return 'LastURL=' . $url;
+            return 'LastURL=';
         }
     }
 
